5668ba37b4589ab109f3194639ba5b2f7d987fff
Thomas FALCHER <tfalcher.externe@pagesjaunes.fr>
2017-08-02 18:05:33 +0200

    [TECH] Modification des scripts de lancement/init

--- cabot-notifier_startup.sh
+++ cabot-notifier_startup.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 nvm use lts/boron
-pm2 delete INT_cabot-notifier PROD_cabot-notifier
-CABOTNOTIFIER_ENV=int pm2 start /home/mdrouian/Integration/Projects/cabot-notifier/app.js --name INT_cabot-notifier
-CABOTNOTIFIER_ENV=prod pm2 start /home/mdrouian/Integration/Projects/cabot-notifier/app.js --name PROD_cabot-notifier
+#pm2 delete INT_cabot-notifier PROD_cabot-notifier
+
+# Install nodes_modules
+cd INT_cabot-notifier/
+npm install
+cd ..
+cd PROD_cabot-notifier/
+npm install
+
+# Lancement des instances
+CABOTNOTIFIER_ENV=int pm2 start /home/tfalcher/devs/tools/cabot-notifier/INT_cabot-notifier/app.js --name INT_cabot-notifier
+CABOTNOTIFIER_ENV=prod pm2 start /home/tfalcher/devs/tools/cabot-notifier/PROD_cabot-notifier/app.js --name PROD_cabot-notifier
 
